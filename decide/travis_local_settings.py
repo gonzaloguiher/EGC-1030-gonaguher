54b8a6ed36eaf7620b5463dd3e6deb945429a306
gonzaloguiher <zalo.agui1@gmail.com>
2019-12-26 20:29:26 +0100

    Configuración de travis 3.0

--- decide/travis_local_settings.py
+++ decide/travis_local_settings.py
@@ -2,27 +2,27 @@ ALLOWED_HOSTS = ["*"]
 
 # Modules in use, commented modules that you won't use
 MODULES = [
-   # 'authentication',
-   # 'base',
-   # 'booth',
-   # 'census',
+    'authentication',
+    'base',
+    'booth',
+    'census',
     'mixnet',
-   # 'postproc',
-   # 'store',
-   # 'visualizer',
-   # 'voting',
+    'postproc',
+    'store',
+    'visualizer',
+    'voting',
 ]
 
 APIS = {
-   # 'authentication': 'http://localhost:8000',
-   # 'base': 'http://localhost:8000',
-   # 'booth': 'http://localhost:8000',
-   # 'census': 'http://localhost:8000',
+    'authentication': 'http://localhost:8000',
+    'base': 'http://localhost:8000',
+    'booth': 'http://localhost:8000',
+    'census': 'http://localhost:8000',
     'mixnet': 'http://localhost:8000',
-   # 'postproc': 'http://localhost:8000',
-   # 'store': 'http://localhost:8000',
-   # 'visualizer': 'http://localhost:8000',
-   # 'voting': 'http://localhost:8000',
+    'postproc': 'http://localhost:8000',
+    'store': 'http://localhost:8000',
+    'visualizer': 'http://localhost:8000',
+    'voting': 'http://localhost:8000',
 }
 
 BASEURL = 'http://10.5.0.1:8000'
